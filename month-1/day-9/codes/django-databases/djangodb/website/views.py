32668b58d884133c53e3b67bec857eb207e66f2d
shafquatulbari <77532312+shafquatulbari@users.noreply.github.com>
2024-10-24 17:00:33 +0600

    pulling data from db, create and process forms

--- month-1/day-9/codes/django-databases/djangodb/website/views.py
+++ month-1/day-9/codes/django-databases/djangodb/website/views.py
@@ -1,5 +1,23 @@
 from django.shortcuts import render
+from .models import Member
+from .forms import MemberForm
 
 # Create your views here.
 def home(request):
-    return render(request, "home.html", {}) #context dictionary is empty
+    #get all the members from the database
+    all_members = Member.objects.all()
+    #pass the members to the template
+    return render(request, "home.html", {"all": all_members})
+
+def join(request):
+    #if the form is submitted
+    if request.method == "POST":
+        form = MemberForm(request.POST or None)
+        #if the form is valid, we need the validation to be done
+        if form.is_valid(): 
+            #save the form to the database
+            form.save()
+        #redirect to the home page
+        return render(request, "join.html", {})    
+    else:
+        return render(request, "join.html", {})
